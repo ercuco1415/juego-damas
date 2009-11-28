7c34d615c9993ef8fa081cae73627aa6573c699c
alejandrolarosa <alejandrolarosa@71c2b8b2-bea8-11de-869a-473eb4776397>
2009-11-28 04:14:52 +0000

    clases para persistencia.

---
+++ juego-damas-core-java/src/excepciones/FormatoCasilleroException.java
@@ -0,0 +1,13 @@
+package excepciones;
+
+public class FormatoCasilleroException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1260668309317564646L;
+
+	public FormatoCasilleroException(String str){
+		super(str);
+	}
+}
